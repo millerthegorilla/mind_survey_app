60cdbe18cc1a48f5f87f81a58a61f4cc13cc0cc4
James Miller <jamesstewartmiller@gmail.com>
2026-01-04 22:09:25 +0000

    updated dependencies and edited compose file

--- config/settings/base.py
+++ config/settings/base.py
@@ -153,7 +153,7 @@ STATIC_ROOT = str(BASE_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [str(APPS_DIR / "static")]
+STATICFILES_DIRS = [str(APPS_DIR / "static/mind_survey_app")]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
