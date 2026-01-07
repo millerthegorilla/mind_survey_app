15969aa4881c9b60c6100803b84eacb08a04b89e
James Miller <jamesstewartmiller@gmail.com>
2026-01-07 18:52:48 +0000

    updated uv.lock, reimplemented add_10_random_users in user admin.py to add and verify email addresses

--- mind_survey_app/users/admin.py
+++ mind_survey_app/users/admin.py
@@ -1,8 +1,12 @@
-from allauth.account.decorators import secure_admin_login
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.utils.translation import gettext_lazy as _
+from django.shortcuts import redirect
+from allauth.account.decorators import secure_admin_login
+from allauth.account.models import EmailAddress
+from faker import Faker
+from djf_surveys.admin import UserAdmin
 
 from .forms import UserAdminChangeForm
 from .forms import UserAdminCreationForm
@@ -16,7 +20,7 @@ if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
 
 
 @admin.register(User)
-class UserAdmin(auth_admin.UserAdmin):
+class LocalUserAdmin(UserAdmin):
 
     form = UserAdminChangeForm
     add_form = UserAdminCreationForm
@@ -39,7 +43,24 @@ class UserAdmin(auth_admin.UserAdmin):
     )
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
-
+    
+    def add_10_random_users(self, request):
+        breakpoint()
+        fake = Faker()
+        ids = []
+        for index in range(10):
+            user = User()
+            user.username = fake.passport_number()
+            user.password = fake.password()
+            try:
+               user.save()
+            except IntegrityError:
+                index -= 1
+                continue
+            email = EmailAddress.objects.create(user=user, email=fake.email(), verified=True)
+            email.save()
+            ids.append(user.id)
+        return redirect("admin:show_added_users", str(ids))
 
 #  Perhaps put first_name and last_name back to none in models if the below is added.
 #     change_list_template = "djf_surveys/admin/change_list.html"
